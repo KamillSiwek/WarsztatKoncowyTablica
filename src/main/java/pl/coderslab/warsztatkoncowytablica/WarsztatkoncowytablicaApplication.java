f51d50e00d3f47ac9c45f277b2cf62d196266aca
Kamil Siwek <kamillsiwek@gmail.com>
2019-01-05 10:51:39 +0100

    update

---
+++ src/main/java/pl/coderslab/warsztatkoncowytablica/WarsztatkoncowytablicaApplication.java
@@ -0,0 +1,14 @@
+package pl.coderslab.warsztatkoncowytablica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarsztatkoncowytablicaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WarsztatkoncowytablicaApplication.class, args);
+    }
+
+}
+
